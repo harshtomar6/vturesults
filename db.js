3094f9a17199cdec33c9f25394597dbb811fbd94
Harsh Tomar <harshtomar6@gmail.com>
2018-02-15 21:39:12 +0530

    Fixed

--- db.js
+++ db.js
@@ -10,7 +10,8 @@ let User = mongoose.model('User', userSchema);
 let saveUser = (data, callback) => {
   let user = new User(data);
   User.find({email: data.email}, (err, doc) => {
-    if(doc)
+    console.log(doc)
+    if(doc.length > 1)
       return callback('Already Subscribed', false)
     else
       user.save((err, success) => {
